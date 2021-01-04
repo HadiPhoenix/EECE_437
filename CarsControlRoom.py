4949d2902d348498b4f1c86043443b0202f47755
HadiPhoenix <76945480+HadiPhoenix@users.noreply.github.com>
2021-01-04 12:38:12 +0200

    Update CarsControlRoom.py
    by Hadi

--- CarsControlRoom.py
+++ CarsControlRoom.py
@@ -7,12 +7,13 @@ class Cars_Control_Room(Tk):
         self.geometry('500x500')
         self.title("Cars Control Room")
         self.DB_filepath = DB_filepath
-        self.conn = sqlite3.connect(self.DB_filepath)
+        self.conn = sqlite3.connect(self.DB_filepath) #connecting to the DB
         self.c = self.conn.cursor()
         self.limit = 10
         self.check_cars()
         self.mainloop()
     def Dropped_Off_Passenger(self, Status, Ride_ID, ID_Num):
+        #Function responsible to check if the passenger is dropped off and change cars' statuses accordingly
 
         if Status == 'Done':
             # Go to CarsLinkedToUsersDB and change the ride status to 'Done' and then go to CarsDB and put Car status as 'Available'
@@ -25,16 +26,18 @@ class Cars_Control_Room(Tk):
             pass
 
     def Send_Car_To_Maintenance_Gasoline(self, Notification, CarID):
-        if Notification == 2:
-            Cars(CarID).GoTo_Maitenance_Gasoline('Garage then Gas Station')
-        elif Notification == 1:
+        #Function reponsible to simulate the deplacement of cars to certain places depending on its status on fuel and mechanics
+        if Notification == 2: #Notification 2 is for both mechanic and fuel needs
+            Cars(CarID).GoTo_Maitenance_Gasoline('Garage then Gas Station') #Send car to both garage and gas station
+        elif Notification == 1: #Notification 1 is for mechanic needs
             Cars(CarID).GoTo_Maitenance_Gasoline('Garage')
-        elif Notification == 0:
+        elif Notification == 0: #Notification 0 is for fuel needs
             Cars(CarID).GoTo_Maitenance_Gasoline('Gas Station')
         else:
             pass
 
     def check_cars(self):
+        #Function responsible to handle pending requests and timeouts and changing the cars' statuses in accordance
         self.c.execute('SELECT * FROM CarsLinkedToUsers WHERE STATUS = ?' , ('Pending',))
         car = self.c.fetchall()
         if len(car) == 0:
@@ -42,9 +45,9 @@ class Cars_Control_Room(Tk):
         else:
             for temp in car:
                 if time.time() - temp[7] >= self.limit:
-                    self.c.execute("DELETE FROM CarsLinkedToUsers WHERE CAR_ID_NUM = ? AND STATUS = ?" , (temp[1], 'Pending'))
+                    self.c.execute("DELETE FROM CarsLinkedToUsers WHERE CAR_ID_NUM = ? AND STATUS = ?" , (temp[1], 'Pending')) #Delete request if timeout occured
                     self.conn.commit()
-                    self.c.execute('UPDATE Cars SET STATUS = ? WHERE ID_NUM = ?', ("Available", temp[1]))
+                    self.c.execute('UPDATE Cars SET STATUS = ? WHERE ID_NUM = ?', ("Available", temp[1])) #Replace Pending by Available
                     self.conn.commit()
 
             self.after(5000, self.check_cars)
